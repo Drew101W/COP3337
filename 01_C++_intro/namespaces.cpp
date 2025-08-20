58850b2c2e58292875891b142b653a4da01e4fb5
Drew101W <dwhiting9877@floridapoly.edu>
2025-08-20 18:43:45 +0000

    Created namespaces file

---
+++ 01_C++_intro/namespaces.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Create the namespace
+namespace first {
+    int a = 5;
+
+    void print() {
+        std::cout << "First" << std::endl;
+    }
+
+}
+
+namespace second {
+    int a = 5;
+
+    void print() {
+        std::cout << "Second" << std::endl;
+    }
+
+}
+
+int main(void) {
+    first::print();
+    second::print();
+    return 0;
+}
