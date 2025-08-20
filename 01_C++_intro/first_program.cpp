4de98016566aaa3041a907e302741d4c8e3c7213
Drew101W <dwhiting9877@floridapoly.edu>
2025-08-20 18:42:23 +0000

    First C++ program FA25

---
+++ 01_C++_intro/first_program.cpp
@@ -0,0 +1,21 @@
+/*
+*   08/20/2025
+*   The first C++ program
+*
+*
+*/
+
+#include <iostream>
+
+// Create a main function
+
+int main(void) {
+    std::cout  << "Hello COP3337!" << std::endl;
+
+    // std - std namespace
+    // :: - scope resolution operator
+    // cout - console output (object of iostream class)
+    // << - stream insetion operator
+    // endl - end of line
+    return 0;
+}
